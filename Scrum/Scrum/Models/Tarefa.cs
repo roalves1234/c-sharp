b25169143cbd9d6e7042879f2d0917ba20ac8fef
roalves1234 <roalves1234@hotmail.com>
2024-02-15 22:08:24 -0300

    Aplicação da DDD

---
+++ Scrum/Scrum/Models/Tarefa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrum.Model
+{
+    public class Tarefa
+    {
+        public Tarefa()
+        {
+            Data = DateTime.Now.Date;
+            Hora = DateTime.Now.TimeOfDay;
+            Status = "A Fazer";
+        }
+        public virtual int IdTarefa { get; set; }
+        public virtual string Descricao { get; set; }
+        public virtual DateTime Data { get; set; }
+        public virtual TimeSpan Hora { get; set; }
+        public virtual int Pontuacao { get; set; }
+        public virtual string Status { get; set; }
+        public virtual Sprint Sprint { get; set; }
+    }
+}
