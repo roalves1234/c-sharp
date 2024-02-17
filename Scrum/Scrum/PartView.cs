220b98ff19491bdecf9609e6c059a318485f4847
roalves1234 <roalves1234@hotmail.com>
2024-02-17 15:57:24 -0300

    Atualização de PartView para a nova forma

--- Scrum/Scrum/PartView.cs
+++ Scrum/Scrum/PartView.cs
@@ -28,84 +28,26 @@ namespace Scrum
 
     public partial class PartView : UserControl
     {
+        private PartPresenter presenter;
         private Color CorSelecionado = Color.Yellow;
         private Color CorNormal = Color.White;
-        private ISession session;
-        private IList<Tarefa> listaTarefa;
-        private IPartRegra regra;
-        private BindingList<Tarefa> bindGrid;
-        private Tarefa tarefaAtual;
-
-        public PartView(IPartRegra regra, String titulo)
+        public PartView()
         {
             InitializeComponent();
-
-            this.regra = regra;
-            this.session = ConexaoBanco.getInstance().NewSession();
-            this.ListarRegistro();
-            this.Dock = DockStyle.Fill;
-            lblTitulo.Text = titulo;
         }
 
-        public Tarefa TarefaAtual { get { return tarefaAtual; } }
-
-        public void AdicionarTarefa(Tarefa tarefa)
+        public PartView SetPresenter(PartPresenter Value)
         {
-            using (ITransaction transacao = session.BeginTransaction())
-                try
-                {
-                    regra.Alterar(tarefa);
-                    session.Update(tarefa);
-                    transacao.Commit();
-                }
-                catch
-                {
-                    transacao.Rollback();
-                    throw;
-                }
-
-            listaTarefa.Add(tarefa);
-            this.RefreshView();
-            grid.Focus();
-            grid.CurrentCell = grid[0, grid.Rows.Count - 1];
-        }
-
-        public void RemoverTarefaAtual()
-        {
-            session.Evict(tarefaAtual);
-            listaTarefa.Remove(tarefaAtual);
-            this.RefreshView();
-        }
-
-        private void RefreshView()
-        {
-            lblPontos.Text = listaTarefa.Sum(t => t.Pontuacao).ToString();
-            bindGrid.ResetBindings();
-        }
-
-        private void ListarRegistro()
-        {
-            listaTarefa = session
-                             .QueryOver<Tarefa>()
-                             .Where(regra.GetCondicaoWhere())
-                             .OrderBy(t => t.IdTarefa).Asc
-                             .List();
-            grid.DataSource = (bindGrid = new BindingList<Tarefa>(listaTarefa));
-
-            new ManipulacaoGrid(grid)
-                .RedimensionarColunas()
-                .SetNomeColuna("Descricao")
-                .SetTamanho(200);
-            DoDefinirCorSelecionado(false);
-            this.RefreshView();
+            presenter = Value;
+            return (this);
         }
 
         private void grid_SelectionChanged(object sender, EventArgs e)
-        {
+        {          
             if ((grid.SelectedRows.Count > 0) && (grid.SelectedRows[0].DataBoundItem != null) && (grid.SelectedRows[0].DataBoundItem.GetType() == typeof(Tarefa)))
-                tarefaAtual = (grid.SelectedRows[0].DataBoundItem as Tarefa);
+                presenter.SetTarefaAtual((grid.SelectedRows[0].DataBoundItem as Tarefa));
             else
-                tarefaAtual = null;
+                presenter.SetTarefaAtual(null);
         }
 
         private void DefinirCorSelecionado(Color cor)
@@ -134,15 +76,6 @@ namespace Scrum
         private void grid_Leave(object sender, EventArgs e)
         {
             DoDefinirCorSelecionado(false);
-        }
-        public static void Transferir(PartView origem, PartView destino)
-        {
-            if (origem.TarefaAtual != null)
-            {
-                Tarefa tarefa = origem.TarefaAtual;
-                origem.RemoverTarefaAtual();
-                destino.AdicionarTarefa(tarefa);
-            }
         }
     }
 }
